4dd997ca58a369045c71d88d45d7889dccf10689
PauloMarxs <paulomarquestelles@gmail.com>
2021-03-04 16:16:15 -0300

    correções

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,8 +1,24 @@
+import React from "react";
 import styled from "styled-components";
+import gitImg from "../../img/github.png";
 
-const MainContent = styled.div`
+const StyledMainContent = styled.div`
   margin: 40px 240px;
   font-family: "Roboto", sans-serif;
+  position: relative;
+
+  &::before {
+    position: absolute;
+    content: "";
+    background: url(${gitImg});
+    width: 688px;
+    height: 688px;
+    right: 0;
+    top: -40px;
+    z-index: -1;
+  }
 `;
 
-export default MainContent;
+export default function MainContent({ children }) {
+  return <StyledMainContent>{children}</StyledMainContent>;
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import MainContent from "./components/MainContent";
 import "./index.css";
 import Header from "./components/Header";
 import UserSearchBar from "./components/UserSearchBar";
+import CardArea from "./components/CardArea";
 
 ReactDOM.render(
   <React.StrictMode>
     <MainContent>
       <Header></Header>
       <UserSearchBar></UserSearchBar>
+      <CardArea></CardArea>
     </MainContent>
   </React.StrictMode>,
   document.getElementById("root")
