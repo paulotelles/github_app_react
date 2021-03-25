fae1e022516e6ac0b1b24550408e75780d967c85
PauloMarxs <paulomarquestelles@gmail.com>
2021-03-25 16:41:01 -0300

    criação de um módulo de mensagem de erro

---
+++ src/components/ErrorMessage/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ErrorMsg = styled.div`
+  width: 400px;
+  height: 50px;
+  padding: 10px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const ErrorText = styled.p`
+  font-size: 18px;
+  color: red;
+`;
